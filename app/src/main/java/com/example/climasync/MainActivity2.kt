a7f0805e28beca073c454ea1b6f62b87da66a4d3
Carlos Ramos Archundia <120069919+ArchundiaXP@users.noreply.github.com>
2025-03-27 19:24:50 -0600

    Creación de la vista Layout login en fragment_first

--- app/src/main/java/com/example/climasync/MainActivity2.kt
+++ app/src/main/java/com/example/climasync/MainActivity2.kt
@@ -12,13 +12,13 @@ import com.example.climasync.databinding.ActivityMain2Binding
 class MainActivity2 : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMain2Binding
+private lateinit var binding: ActivityMain2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMain2Binding.inflate(layoutInflater)
-        setContentView(binding.root)
+     binding = ActivityMain2Binding.inflate(layoutInflater)
+     setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
 
@@ -28,14 +28,14 @@ class MainActivity2 : AppCompatActivity() {
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+                    .setAction("Action", null)
+                    .setAnchorView(R.id.fab).show()
         }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    val navController = findNavController(R.id.nav_host_fragment_content_main)
+    return navController.navigateUp(appBarConfiguration)
+            || super.onSupportNavigateUp()
     }
 }
