7333f4e517b76743088a0d14c0fb7fe310ff746f
Citlally Suarez <citlally.suarez@comunidad.unam.mx>
2025-04-28 18:57:35 -0600

    Se agrego Retrofit y se implemento el modelo del Api

---
+++ app/src/main/java/com/example/climasync/model/WeatherResponse.kt
@@ -0,0 +1,37 @@
+package com.example.climasync.model
+
+import com.google.gson.annotations.SerializedName
+
+
+data class WeatherResponse (
+    val location: Location,
+    val current: Current
+)
+
+data class Location(
+
+    val name: String,
+    val region: String,
+    val country: String,
+    val lat: Double,
+    val lon: Double,
+    @SerializedName("tz_id") val tzId: String,
+    val localTime: String
+)
+
+data class Current(
+    @SerializedName("temp_c") val tempC: Double,
+    @SerializedName("temp_f") val tempF: Double,
+    val condition: Condition,
+    @SerializedName("wind_kph") val windKph: Double,
+    @SerializedName("wind_degree") val windDegree: Int,
+    @SerializedName("wind_dir") val windDir: String,
+    @SerializedName("humidity")
+    val humidity: Int,
+    @SerializedName("feelslike_c") val feelsLikeC: Double
+)
+
+data class Condition(
+    val text: String,
+    val icon: String
+)
