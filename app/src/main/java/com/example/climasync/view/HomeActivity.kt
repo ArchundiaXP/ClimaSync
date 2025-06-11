ff81abe89ce54b60a1bfbb4b0417d8fd23e4d338
GioZaam51 <150202907+GioZaam51@users.noreply.github.com>
2025-06-10 20:07:10 -0600

    barra navegación

--- app/src/main/java/com/example/climasync/view/HomeActivity.kt
+++ app/src/main/java/com/example/climasync/view/HomeActivity.kt
@@ -3,18 +3,33 @@ package com.example.climasync.view
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.climasync.R
 import com.example.climasync.databinding.ActivityHomeBinding
 import com.example.climasync.utils.FragmentCommunicator
 
 class HomeActivity : AppCompatActivity(), FragmentCommunicator {
 
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding= ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        //bottomNavigationView con el navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
     override fun showLoader(value: Boolean) {
